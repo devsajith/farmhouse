c9f59dedb9ba81acf9a91ee0840ddb7ca414d912
Arjun81951 <arjun81951@gmail.com>
2026-01-16 12:25:44 +0530

    4

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -2,7 +2,7 @@ export default function Projects() {
   return (
     <div style={{ padding: "100px", color: "white" }}>
       <h1>My Projects 🚀</h1>
-      <p>AdminGuard, CampusGuard, SMART HIRE, etc...</p>
+      <p>AdminGuard, CampusGuard, SMART HIRE etc...</p>
     </div>
   );
 }
